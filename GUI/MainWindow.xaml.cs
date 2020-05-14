f2f5d77f300740cda0ab0f1e56e422320a8bb8cf
Oliver Tworkowski <olitworkowski@gmail.com>
2020-05-14 13:51:41 +0200

    add Settingsformular und Datagrid testing

--- GUI/MainWindow.xaml.cs
+++ GUI/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,39 @@ namespace GUI
         public MainWindow()
         {
             InitializeComponent();
+            fillPP();
+        }
+
+        public void fillPP()
+        {
+            DataTable pp = new DataTable();
+
+            List<DataColumn> cols = new List<DataColumn>();
+
+            cols.Add(new DataColumn("PAD", typeof(String)));
+            cols.Add(new DataColumn("PAuftr", typeof(String)));
+            cols.Add(new DataColumn("PDatum", typeof(DateTime)));
+            cols.Add(new DataColumn("loeschDatum", typeof(DateTime)));
+            cols.Add(new DataColumn("PArt", typeof(String)));
+
+            //DataColumn datum = new DataColumn("PDatum", typeof(DateTime));
+            //DataColumn loeschDatum = new DataColumn("loeschDatum", typeof(DateTime));
+            //DataColumn art = new DataColumn("PArt", typeof(String));
+
+            foreach(var dc in cols)
+            {
+                pp.Columns.Add(dc);
+            }
+
+            DataRow rowTest = pp.NewRow();
+            rowTest[0] = "1z0825z080";
+            rowTest[1] = "Auftrag";
+            rowTest[2] = new DateTime(2020, 12, 24);
+            rowTest[3] = new DateTime(2020, 3, 12);
+            rowTest[4] = "Eine Art";
+
+            pp.Rows.Add(rowTest);
+            PP_TABELLE.ItemsSource = pp.DefaultView;
         }
     }
 }
