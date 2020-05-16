900602a172acc13b889e0c2985052fb11af91b10
Yannik Sahl <s0569340@htw-berlin.de>
2020-05-16 14:05:15 +0200

    Added DBHandler setup, DBConnection

---
+++ DBHandler/Program.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace DBHandler
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.Write("test");
+
+
+        }
+
+    }
+}
+
