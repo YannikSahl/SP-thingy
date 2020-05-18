bad82a4f8bc9af93233a7210c4405d1ace0a29c0
Yannik Sahl <s0569340@htw-berlin.de>
2020-05-18 17:50:28 +0200

    Transferred prior changes

--- DBHandler/DBConnection.cs
+++ DBHandler/DBConnection.cs
@@ -2,9 +2,48 @@
 using System.Collections.Generic;
 using System.Text;
 
+
+enum StatusCode
+{
+    CommandOK = 0,
+    CommandFailed = 1,
+    ConnectionFailed = 2
+}
+
 namespace DBHandler
 {
-    class DBConnection
+    public class DBConnection
     {
+
+        // Member variables
+        String downloadLocation;
+        //SPH.SharePointHandler SPHandler;
+
+        // Constructor
+        public DBConnection(String SharePointPath, String apikey, String downloadLocation)
+        {
+
+            // Save download location for further use
+            this.downloadLocation = downloadLocation;
+
+            // Create SPHandler to connect to sharepoint
+            //this.SPHandler = new SPH.SharePointHandler(SharePointPath, apikey, downloadLocation);
+
+        }
+
+
+        // Open .accdb file
+        StatusCode openDBFile()
+        {
+
+            string connString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=E:\\Benutzer\\Yannik\\NcDrive\\HTW\\Module\\4 (5) Softwareentwicklungsprojekt\\Team 5\\Gegeben\\Datenmodell;";
+
+
+            return StatusCode.CommandOK;
+
+        }
+
+
     }
+
 }
