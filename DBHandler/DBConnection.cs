6491cf3a474312fbab3cbfb8ce2b46db34b12e3f
Yannik Sahl <s0569340@htw-berlin.de>
2020-05-18 17:48:32 +0200

    Re-setup DBHandler project (went from target framework .NET Standard to .NET Core)

--- DBHandler/DBConnection.cs
+++ DBHandler/DBConnection.cs
@@ -1,43 +1,10 @@
 ﻿using System;
-using SPH = SharePointHandler;
-using System.Data.OleDb;
-
-
-enum Statuscode
-{
-    CommandOK = 0,
-    CommandFailed = 1,
-    ConnectionFailed = 2
-}
+using System.Collections.Generic;
+using System.Text;
 
 namespace DBHandler
 {
-    public class DBConnection
+    class DBConnection
     {
-
-        // Member variables
-        String downloadLocation;
-        SPH.SharePointHandler SPHandler;
-
-        // Constructor
-        public DBConnection(String SharePointPath, String apikey, String downloadLocation)
-        {
-
-            // Save download location for further use
-            this.downloadLocation = downloadLocation;
-
-            // Create SPHandler to connect to sharepoint
-            this.SPHandler = new SPH.SharePointHandler(SharePointPath, apikey, downloadLocation);
-
-        }
-
-
-        // Verbindung zu SharePoint
-        string connString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\\source\\SiteCore65\\Individual-Data.xls;Extended Properties=Excel 8.0;";
-
-
-        // Öffnen .accdb file
-
-
     }
 }
