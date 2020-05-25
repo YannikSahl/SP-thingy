55144fcc38f5e996d4d2bc172fad56e4f2e2b86b
Yannik Sahl <s0569340@htw-berlin.de>
2020-05-25 15:00:27 +0200

    Managed to read data from database

--- DBHandler/DBConnection.cs
+++ DBHandler/DBConnection.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Data.OleDb;
+using System.Data;
 using System.ComponentModel;
 
 public enum StatusCode
@@ -38,20 +39,41 @@ namespace DBHandler
         public StatusCode openDBFile()
         {
 
-            // Test
-            OleDbCommand command = new OleDbCommand("SELECT * FROM PH");
             // string connString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=E:\\Benutzer\\Yannik\\NcDrive\\HTW\\Module\\4 (5) Softwareentwicklungsprojekt\\Team 5\\Gegeben\\Datenmodell;";
             string connString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\\Users\\Yannik\\Desktop\\Datenmodell.accdb;";
 
             using (OleDbConnection connection = new OleDbConnection(connString))
             {
+                
 
-                command.Connection = connection;
+                // Select
+                OleDbCommand commandSelect = new OleDbCommand("SELECT PAD, HStat FROM PH WHERE HStat = '1';", connection);
+                commandSelect = new OleDbCommand("SELECT PAD, HStat FROM PH;", connection);
+                commandSelect.Parameters.Add("HSys", OleDbType.VarChar, 15);
+
+                // Insert
+                OleDbCommand commandInsert = new OleDbCommand("SELECT PAD, HStat FROM PH WHERE HStat = '1';", connection);
+
+                // Delete
+                OleDbCommand commandDelete = new OleDbCommand("SELECT PAD, HStat FROM PH WHERE HStat = '1';", connection);
 
                 try
                 {
+
                     connection.Open();
-                    Console.WriteLine(command.ExecuteNonQuery());
+
+                    // Print schema info
+                    DataTable table = connection.GetSchema();
+
+                    // Execute commands
+                    OleDbDataReader reader = commandSelect.ExecuteReader(); //Console.WriteLine(commandSelect.ExecuteNonQuery());
+                    Console.WriteLine("Field count: " + reader.FieldCount);
+
+                    // Read output
+                    while (reader.Read())
+                    {
+                        Console.WriteLine(reader[0].ToString());
+                    }
                 }
                 catch(Exception ex)
                 {
