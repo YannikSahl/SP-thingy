44ad9af3dbe02fb4e43de9632a80d506b1eb682f
Oliver Tworkowski <olitworkowski@gmail.com>
2020-06-18 14:16:05 +0200

    Settingspaths Textboxen und Logik erstellen

--- GUI/Settings.xaml.cs
+++ GUI/Settings.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -10,6 +11,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using SPHandler;
+using Path = System.IO.Path;
 
 namespace GUI
 {
@@ -21,6 +23,7 @@ namespace GUI
         public Settings()
         {
             InitializeComponent();
+            DbSave.Text = Path.GetFullPath("..\\..\\..\\..\\DBHandler");
         }
 
         public void saveAuth()
@@ -39,6 +42,40 @@ namespace GUI
                 sp_test.Background = new SolidColorBrush(Color.FromRgb(20, 200, 20));
             else
                 sp_test.Background = new SolidColorBrush(Color.FromRgb(200,20,20));
+        }
+
+        private void TexBoxLinkEnter(object sender, RoutedEventArgs e)
+        {
+            var children = ((DockPanel) ((Button) sender).Parent).Children;
+            foreach (var child in children)
+            {
+                var pSender = child as TextBox;
+                if (pSender == null)
+                    continue;
+
+                var dir = Path.GetFullPath(pSender.Text);
+                if (Directory.Exists(dir))
+                {
+                    pSender.Foreground = new SolidColorBrush(Colors.ForestGreen);
+                    return;
+                }
+                else
+                {
+                    pSender.Foreground = new SolidColorBrush(Colors.Red);
+                    MessageBox.Show("Kein gültiger Pfad", "Bitte gib einen gültigen Pfad ein", MessageBoxButton.OK);
+                    return;
+                }
+            }
+        }
+
+        private void SelectDirectory(object sender, RoutedEventArgs e)
+        {
+            // https://stackoverflow.com/questions/1922204/open-directory-dialog
+
+            //using (var dialog = new System.Windows.Forms.FolderBrowserDialog())
+            //{
+            //    System.Windows.Forms.DialogResult result = dialog.ShowDialog();
+            //}
         }
     }
 }
