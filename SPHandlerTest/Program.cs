1c2c6ea5df57a7f11d694722ce8bb5a1485cdf1b
Lukas <s0568476@htw-berlin.de>
2020-06-25 19:42:56 +0200

    trying to solve 401 unauthorized

--- SPHandlerTest/Program.cs
+++ SPHandlerTest/Program.cs
@@ -23,27 +23,31 @@ namespace SharePointTryOut
 
             using (var ctx = new ClientContext(rootSite))
             {
-                ctx.Credentials = new SharePointOnlineCredentials(username, password);
+                CredentialCache cc = new CredentialCache();
+                SharePointOnlineCredentials Creds = new SharePointOnlineCredentials(username, password);
+                ctx.Credentials = Creds;
                 var web = ctx.Web;
                 ctx.Load(web);
                 ctx.ExecuteQueryAsync().Wait();
 
 
-                DownloadFile(sourceSite + folderSite + fileSite,ctx.Credentials, destinationPath);
+                DownloadFile(rootSite+ sourceSite + folderSite + fileSite,Creds, destinationPath);
             }
 
             Console.ReadLine();
         }
 
-        private static void DownloadFile(string webUrl, ICredentials credentials, string destinationPath)
+        private static void DownloadFile(string webUrl, SharePointOnlineCredentials credentials, string destinationPath)
         {
             using (var client = new WebClient())
             {
+                client.UseDefaultCredentials = true;
                 client.Headers.Add("X-FORMS_BASED_AUTH_ACCEPTED", "f");
                 client.Headers.Add("User-Agent: Other");
                 client.Credentials = credentials;
                 client.DownloadFile(webUrl, destinationPath);
             }
+            
         }
 
         public static string GetFileUrlFromDB(int entry, bool type)
