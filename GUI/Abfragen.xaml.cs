7b01b7c7db50b35ae725ae9659036dc2e045022f
Oliver Tworkowski <olitworkowski@gmail.com>
2020-06-26 14:38:07 +0200

    abfrageformular saved queries verbessern

--- GUI/Abfragen.xaml.cs
+++ GUI/Abfragen.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Data;
 using System.Runtime.CompilerServices;
 using System.Security.Cryptography;
@@ -22,6 +23,14 @@ namespace GUI
     /// </summary>
     public partial class Abfragen : Window
     {
+        private enum abfrageTypen
+        {
+            [Description("SQL")]
+            Sql = 1,
+            [Description("Text Parameter")]
+            Parameter = 2
+        }
+
         private MainWindow _mainWindow;
 
         public Abfragen(MainWindow mw)
@@ -37,9 +46,16 @@ namespace GUI
             dt.Columns.Add(new DataColumn("Strecke", typeof(string)));
             dt.Columns.Add(new DataColumn("Auftrag", typeof(string)));
             dt.Columns.Add(new DataColumn("Punktart", typeof(string)));
-            var hashCol = new DataColumn("Hash", typeof(string));
-            //hashCol.ColumnMapping = MappingType.Hidden;
-            dt.Columns.Add(hashCol);
+            dt.Columns.Add(new DataColumn("von_km", typeof(string)));
+            dt.Columns.Add(new DataColumn("bis_km", typeof(string)));
+            dt.Columns.Add(new DataColumn("von_Punkt", typeof(string)));
+            dt.Columns.Add(new DataColumn("bis_Punkt", typeof(string)));
+            dt.Columns.Add(new DataColumn("von_Blatt", typeof(string)));
+            dt.Columns.Add(new DataColumn("bis_Blatt", typeof(string)));
+            dt.Columns.Add(new DataColumn("Operator", typeof(string)));
+            dt.Columns.Add(new DataColumn("SQL", typeof(string)));
+            dt.Columns.Add(new DataColumn("AbfrageTyp", typeof(abfrageTypen)));
+            dt.Columns.Add(new DataColumn("Hash", typeof(string)));
             SavedQueriesGrid.DataContext = dt;
         }
 
@@ -65,23 +81,57 @@ namespace GUI
             return sBuilder.ToString();
         }
 
+        private string HashFromRow(DataRow dr)
+        {
+            StringBuilder s = new StringBuilder();
+            foreach (var item in dr.ItemArray)
+            {
+                s.Append(item.ToString());
+            }
+
+            var msg = s.ToString();
+            var hash = GetHash(MD5.Create(), msg);
+            return hash;
+        }
+
         private void AddSavedQueryButton_Click(object sender, RoutedEventArgs e)
         {
-            string hashMessage = $"{StreckeInput.Text}{AuftragInput.Text}{PunktartInput.Text}";
-            if ("".Equals(hashMessage))
-                return;
-            string hash = GetHash(MD5.Create(), hashMessage);
+            //string hashMessage = $"{StreckeInput.Text}{AuftragInput.Text}{PunktartInput.Text}{}";
+            //if ("".Equals(hashMessage))
+            //    return;
+            //string hash = GetHash(MD5.Create(), hashMessage);
             DataTable dt = (DataTable) SavedQueriesGrid.DataContext;
             DataRow dr = dt.NewRow();
+            // TODO: Das alles muss in methoiden verpackt werden
             dr[0] = StreckeInput.Text;
             dr[1] = AuftragInput.Text;
             dr[2] = PunktartInput.Text;
-            dr[3] = hash;
+            // von km
+            dr[3] = VonKm.Text;
+            dr[4] = BisKm.Text;
+            // Punkt
+            dr[5] = VonPunkt.Text;
+            dr[6] = BisPunkt.Text;
+            // Blatt
+            dr[7] = VonBlatt.Text;
+            dr[8] = BisBlatt.Text;
+            // Operator
+            var isAndOperator = AndOp.IsChecked.HasValue ? AndOp.IsChecked.Value : false;
+            dr[9] = isAndOperator ? "AND" : "OR";
+            // SQL Text
+            dr[10] = RawQuery.Text;
+            // Query Typ
+            var queryByText = QueryInA.IsChecked.HasValue ? QueryInA.IsChecked.Value : false;
+            dr.ItemArray[11] = queryByText ? abfrageTypen.Parameter : abfrageTypen.Sql;
+            // Hash
+            string hash = HashFromRow(dr);
+            dr[12] = hash;
+
             var exists = false;
             // check if row exists
             foreach (DataRow row in dt.Rows)
             {
-                if (row.ItemArray[3].Equals(hash))
+                if (row.ItemArray[12].Equals(hash))
                 {
                     exists = true;
                     break;
@@ -132,7 +182,7 @@ namespace GUI
             }else
             {
                 // Query by raw SQL
-                //queryExecuted = dbh.FillInData(RawQuery.Text);
+                queryExecuted = dbh.FillInData(RawQuery.Text);
             }
 
             if (queryExecuted == StatusCode.CommandOk)
@@ -158,6 +208,61 @@ namespace GUI
             //{
             //    return;
             //}
+        }
+
+        private void LoadSavedQueryButton_Click(object sender, RoutedEventArgs e)
+        {
+            //DataTable dt = (DataTable)SavedQueriesGrid.DataContext;
+            var selected = SavedQueriesGrid.SelectedItem as DataRowView;
+            if (selected == null)
+                return;
+            DataRow dr = selected.Row;
+            // TODO: string conversion error wenn cell = null
+            // TODO: nach jedem Cell edit muss der hash neu berechnet werden
+            StreckeInput.Text = (string)dr[0];
+            AuftragInput.Text = (string)dr[1];
+            PunktartInput.Text = (string)dr[2];
+            // von km
+            VonKm.Text = (string)dr[3];
+            BisKm.Text = (string)dr[4];
+            // Punkt
+            VonPunkt.Text = (string)dr[5];
+            BisPunkt.Text = (string)dr[6];
+            // Blatt
+            VonBlatt.Text = (string)dr[7];
+            BisBlatt.Text = (string)dr[8];
+            // Operator
+            var isAndOperator = AndOp.IsChecked.HasValue ? AndOp.IsChecked.Value : false;
+            if ("or".Equals(dr.ItemArray[9].ToString().ToLower()))
+            {
+                OrOp.IsChecked = true;
+            }
+            else
+            {
+                AndOp.IsChecked = true;
+            }
+            // SQL Text
+            RawQuery.Text = (string)dr[10];
+            // Query Typ
+            if (abfrageTypen.Parameter.Equals((abfrageTypen)dr[11]))
+            {
+                QueryInA.IsChecked = true;
+            }
+            else
+            {
+                QueryInB.IsChecked = true;
+            }
+        }
+
+        private void SavedQueriesGrid_CellEditEnding(object sender, DataGridCellEditEndingEventArgs e)
+        {
+            var pSender = sender as DataGrid;
+            if (pSender == null)
+                return;
+            var dr = pSender.SelectedItem as DataRowView;
+            if (dr == null)
+                return;
+            dr.Row[12] = HashFromRow(dr.Row);
         }
     }
 }
