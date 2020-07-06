c71591a4a140a4f2a758da0687dbdfa699b5776d
Oliver Tworkowski <olitworkowski@gmail.com>
2020-07-07 01:15:42 +0200

    code quality GUI, readme icon links

--- GUI/Abfragen.xaml.cs
+++ GUI/Abfragen.xaml.cs
@@ -1,22 +1,11 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
-using System.Diagnostics.Eventing.Reader;
-using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Security.Cryptography;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using DBHandler;
 
 namespace GUI
 {
--- GUI/FileView.cs
+++ GUI/FileView.cs
@@ -1,29 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Drawing;
 using System.Globalization;
 using System.IO;
-using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
 using Color = System.Drawing.Color;
+using ColorConverter = System.Windows.Media.ColorConverter;
 using File = System.IO.File;
-using SystemColors = System.Drawing.SystemColors;
+using Point = System.Windows.Point;
 
 namespace GUI
 {
     class FileView : DockPanel
     {
-        //private static Dictionary<string, SolidColorBrush> presetList = new Dictionary<string, SolidColorBrush>
-        //{
-        //    {".jpg", new SolidColorBrush(Colors.Red)},
-        //    {".pptx", new SolidColorBrush(Colors.DodgerBlue)},
-        //    {".png", new SolidColorBrush(Colors.Orange)},
-        //    {".pdf", new SolidColorBrush(Colors.DarkRed)}
-        //};
+        #region statics
+
         private static Dictionary<string, LinearGradientBrush> _presetList;
 
         public enum View
@@ -32,10 +26,14 @@ namespace GUI
             TextAndImage
         }
 
+        #endregion
+
+        #region members
+
         public static View _view = View.TextAndImage;
 
-        //private SolidColorBrush _color;
-        private LinearGradientBrush _color;
+        // 
+        private LinearGradientBrush _iconColor;
         private string _displayText;
         private string _fileExtension;
         private string _filePath;
@@ -45,21 +43,25 @@ namespace GUI
         private SolidColorBrush _baseColor = new SolidColorBrush(Colors.WhiteSmoke);
         private SolidColorBrush _mouseOverColor = new SolidColorBrush(Colors.LightGray);
 
+        #endregion
+
+        #region constructors
+
         public FileView(string path)
         {
             _filePath = Path.GetFullPath(path);
             _directoryPath = Path.GetDirectoryName(_filePath);
             if (File.Exists(_filePath))
             {
+                // init colors
                 _presetList = new Dictionary<string, LinearGradientBrush>();
                 SetColorDict();
 
                 _fileExtension = Path.GetExtension(_filePath);
-                if(! FileView._presetList.TryGetValue(_fileExtension, out _color))
-                    _color = new LinearGradientBrush(Colors.Azure, Colors.Azure, 0);//_color = new SolidColorBrush(Colors.Azure);
+                if(! _presetList.TryGetValue(_fileExtension, out _iconColor))
+                    _iconColor = new LinearGradientBrush(Colors.Azure, Colors.Azure, 0);
                 _fileName = Path.GetFileName(_filePath);
                 _fileName = _fileName.Remove(_fileName.Length - (_fileExtension.Length));
-
                 _displayText = _fileName;
 
                 SetupSelf();
@@ -69,23 +71,43 @@ namespace GUI
             }
             else
             {
+                // file not found
                 _displayText = _filePath + ": Datei existiert nicht!";
             }
 
             AddTextToView();
         }
 
-        private System.Drawing.Color DrawingColorFromHex(string hexColor)
+        #endregion
+
+        #region methods
+
+        /// <summary>
+        /// gets System.Drawing.Color from hex number
+        /// </summary>
+        /// <param name="hexColor"></param>
+        /// <returns></returns>
+        private Color DrawingColorFromHex(string hexColor)
         {
             int argb = Int32.Parse(hexColor.Replace("#", ""), NumberStyles.HexNumber);
             return Color.FromArgb(argb);
         }
 
+        /// <summary>
+        /// gets System.Windows.Media.Color from hex number
+        /// </summary>
+        /// <param name="hexColor"></param>
+        /// <returns></returns>
         private System.Windows.Media.Color MediaColorFromHex(string hexColor)
         {
-            return (System.Windows.Media.Color)System.Windows.Media.ColorConverter.ConvertFromString(hexColor);
+            return (System.Windows.Media.Color)ColorConverter.ConvertFromString(hexColor);
         }
 
+        /// <summary>
+        /// takes a list of Colors (string hex) of length 6 and projects it as a LinearGradientBrush
+        /// </summary>
+        /// <param name="hexColorList"></param>
+        /// <returns></returns>
         private LinearGradientBrush CreateLinearBrushFromColors(string[] hexColorList)
         {
             LinearGradientBrush gradientBrushTemp;
@@ -102,12 +124,15 @@ namespace GUI
             }
 
             gradientBrushTemp = new LinearGradientBrush(gradStopCollTemp);
-            gradientBrushTemp.StartPoint = new System.Windows.Point(0.5, 0);
-            gradientBrushTemp.EndPoint = new System.Windows.Point(0.5, 1);
+            gradientBrushTemp.StartPoint = new Point(0.5, 0);
+            gradientBrushTemp.EndPoint = new Point(0.5, 1);
 
             return gradientBrushTemp;
         }
 
+        /// <summary>
+        /// sets up all the template colors
+        /// </summary>
         private void SetColorDict()
         {
             // PPTX
@@ -167,6 +192,9 @@ namespace GUI
             _presetList.Add(".pdf", pdf_gradBrush);
         }
 
+        /// <summary>
+        /// sets up WPF base class elements
+        /// </summary>
         private void SetupSelf()
         {
             this.LastChildFill = true;
@@ -174,6 +202,9 @@ namespace GUI
             this.Margin = new Thickness(5);
         }
 
+        /// <summary>
+        /// adds a textpreview of the name
+        /// </summary>
         private void AddTextToView()
         {
             var tView = new TextBlock();
@@ -184,9 +215,12 @@ namespace GUI
             tView.HorizontalAlignment = HorizontalAlignment.Left;
             tView.Padding = new Thickness(10,0,0,0);
             this.Children.Add(tView);
-            DockPanel.SetDock(tView, Dock.Left);
+            SetDock(tView, Dock.Left);
         }
 
+        /// <summary>
+        /// adds the icon with color and extension name to base class element
+        /// </summary>
         private void AddImageToView()
         {
             // setup grid
@@ -206,7 +240,7 @@ namespace GUI
             fView.BorderThickness = new Thickness(2);
             fView.BorderBrush = new SolidColorBrush(Colors.DarkGray);
             fView.Margin = new Thickness(5);
-            fView.Background = _color;
+            fView.Background = _iconColor;
             fView.IsHitTestVisible = false;
             grid.Children.Add(fView);
 
@@ -221,18 +255,17 @@ namespace GUI
             grid.Children.Add(fViewText);
 
             this.Children.Add(grid);
-            DockPanel.SetDock(grid, Dock.Right);
+            SetDock(grid, Dock.Right);
         }
 
+        #endregion
 
+        #region events
 
-        /*protected override void OnClick()
-        {
-            //File.Open(_filePath, FileMode.Open, FileAccess.Read, FileShare.None);
-            //System.Diagnostics.Process.Start(@"D:\");
-            base.OnClick();
-        }*/
-
+        /// <summary>
+        /// OnMouseLeftButtonUp event opens directory file is in
+        /// </summary>
+        /// <param name="e"></param>
         protected override void OnMouseLeftButtonUp(MouseButtonEventArgs e)
         {
             // File.Exists() for file
@@ -243,21 +276,26 @@ namespace GUI
             base.OnMouseLeftButtonUp(e);
         }
 
+        /// <summary>
+        /// onMouseEnter event changes color to highlighted
+        /// </summary>
+        /// <param name="e"></param>
         protected override void OnMouseEnter(MouseEventArgs e)
         {
             this.Background = _mouseOverColor;
             base.OnMouseEnter(e);
         }
 
+        /// <summary>
+        /// onMouseLeave event changes color back to normal
+        /// </summary>
+        /// <param name="e"></param>
         protected override void OnMouseLeave(MouseEventArgs e)
         {
             this.Background = _baseColor;
             base.OnMouseLeave(e);
         }
 
-        public string GetFileName()
-        {
-            return _fileName;
-        }
+        #endregion
     }
 }
